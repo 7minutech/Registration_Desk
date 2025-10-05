6801407abb58d5bfd1c6d7afb934f61b05e7bbd6
7minutech <elijohnson1107@gmail.com>
2025-10-05 18:23:34 -0500

    refactor: should use get instead for all for one record

--- routes/attendee.js
+++ routes/attendee.js
@@ -5,22 +5,21 @@ import { badgeNumber, isEnrolled } from '../utils/attendeeHelper.js';
 
 router.get('/:id', (req, resp) => {
     let attendeeID = req.params.id
-    db.all(`SELECT * FROM attendee where attendee._id = ?`, [attendeeID], (err, rows) => {
+    db.get(`SELECT * FROM attendee where attendee._id = ?`, [attendeeID], (err, row) => {
         if (err) {
             console.error(err.message);
         }
-        if (rows.length == 0){
+        if (!row){
             return resp.status(404).json({ error: "Attendee not found"});
         }
-        let attendeeRecord = rows[0];
-        let attendee = {firstname: attendeeRecord.firstname, lastname: attendeeRecord.lastname, displayname: attendeeRecord.displayname}
-        isEnrolled(attendeeRecord._id, (err, enrolled) => {
+        let attendee = {firstname: row.firstname, lastname: row.lastname, displayname: row.displayname}
+        isEnrolled(row._id, (err, enrolled) => {
             if (err){
                 console.error(err.message);
                 return;
             }
             if (enrolled){
-                attendee.badgeNumber = badgeNumber(attendeeRecord._id)
+                attendee.badgeNumber = badgeNumber(row._id)
             }
             resp.json(attendee);
         })
