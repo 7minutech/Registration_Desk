da1059d268ba5b11ab1034ba7cdffb49996f1ee5
7minutech <elijohnson1107@gmail.com>
2025-10-06 13:39:31 -0500

    feat: attendeeForm submission registers the new attendee

--- public/script.js
+++ public/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     $("#attendeeForm").submit(function(e){
         e.preventDefault();
-
+        const attendeesUrl = $("#attendeeForm").attr("action")
         const attendeeData = {
             firstname: $("#firstname").val(),
             lastname: $("#lastname").val(),
             displayname: $("#displayname").val()
         };
         $.ajax({
-            url: $("#attendeeForm").attr("action"),
+            url: attendeesUrl,
             method: "POST",
             data: JSON.stringify(attendeeData),
             contentType: "application/json",
@@ -19,7 +19,29 @@ $(document).ready(function(){
                 } else{
                     alert("Attendee creation failed")
                 }
+            
+                const sessionData = {
+                    sessionID: $("#sessions").val(),
+                }
+                if (sessionData.sessionID == 0 || !(resp.success)){
+                    return
+                }
+                $.ajax({
+                    url: attendeesUrl + `/${resp.attendeeID}/registrations` ,
+                    method: "POST",
+                    data: JSON.stringify(sessionData),
+                    contentType: "application/json",
+                    dataType: "json",
+                    success: function(resp){
+                        if (resp.success){
+                            alert("Attendee registered");
+                        } else{
+                            alert("Attendee registeration failed")
+                        }
+                    }
+                });
             }
         });
+        
     });
 });
